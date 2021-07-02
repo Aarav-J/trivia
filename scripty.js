2abc3af1dbb1fe1cd414eaa84a9245c46fd13bfc
Aarav Jain <aaravjain2007@gmail.com>
2021-07-01 21:31:43 -0400

    Finished Part 2

--- scripty.js
+++ scripty.js
@@ -2,8 +2,15 @@ metal = document.querySelector('#metal')
 adamanatium = document.querySelector("#adamantium")
 vibranium = document.querySelector("#vibranium")
 carbonadium = document.querySelector("#carbonadium")
+correct1 = document.querySelector("#correct1")
+incorrect1 = document.querySelector("#incorrect1")
 
-test = document.querySelector("#test")
+
+submit = document.querySelector("#submit")
+input = document.querySelector("#input")
+correct2 = document.querySelector("#correct2")
+incorrect2 = document.querySelector("#incorrect2")
+
 
 
 
@@ -12,6 +19,8 @@ metal.onclick = function() {
     adamanatium.style.backgroundColor = '#d9edff'
     vibranium.style.backgroundColor = '#d9edff'
     carbonadium.style.backgroundColor = '#d9edff'
+    correct1.style.display = "none"
+    incorrect1.style.display = "block"
 }
 
 
@@ -20,6 +29,9 @@ adamanatium.onclick = function() {
     vibranium.style.backgroundColor = '#d9edff'
     metal.style.backgroundColor = '#d9edff'
     carbonadium.style.backgroundColor = '#d9edff'
+
+    correct1.style.display = "none"
+    incorrect1.style.display = "block"
 }
 
 vibranium.onclick = function() {
@@ -28,6 +40,9 @@ vibranium.onclick = function() {
     metal.style.backgroundColor = '#d9edff'
     adamanatium.style.backgroundColor = '#d9edff'
     carbonadium.style.backgroundColor = '#d9edff'
+
+    correct1.style.display = "block"
+    incorrect1.style.display = "none"
 }
 
 carbonadium.onclick = function() {
@@ -35,9 +50,25 @@ carbonadium.onclick = function() {
     metal.style.backgroundColor = '#d9edff'
     vibranium.style.backgroundColor = '#d9edff'
     adamanatium.style.backgroundColor = '#d9edff'
+    correct1.style.display = "none"
+    incorrect1.style.display = "block"
 }
 
 
-test.addEventListener("submit", function() {
-    console.log("hello")
-})
+submit.onclick = function() {
+    let value = input.value.toLowerCase()
+
+    if(value === "thunder") {
+        incorrect2.style.display = "none";
+        correct2.style.display = "block";
+        input.style.backgroundColor = 'rgb(119, 236, 119) '
+    }
+
+    else
+    {
+        incorrect2.style.display = "block";
+        correct2.style.display = "none";
+
+        input.style.backgroundColor = 'rgb(247, 127, 127)'
+    }
+}
